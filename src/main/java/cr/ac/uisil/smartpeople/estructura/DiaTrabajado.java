fd4fb7a24abae6d0674b04cb999a39ed6fdb581b
TryingToBeComeAProgrammer000 <uyv310599@gmail.com>
2024-06-08 16:32:44 -0600

    Estructura completa

---
+++ src/main/java/cr/ac/uisil/smartpeople/estructura/DiaTrabajado.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.uisil.smartpeople.estructura;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Bernal Santiago
+ */
+public class DiaTrabajado {
+     
+     
+    private long id;
+    LocalDate fecha_Actual = LocalDate.now();
+    private long cantidad_De_Horas_Regulares;
+    private long cantidad_De_Horas_Extra;
+    private float Salario_Por_Horas_Regulares;
+    private float Salario_Por_Horas_Extra;
+    
+    
+}
---
+++ src/main/java/cr/ac/uisil/smartpeople/estructura/Impuesto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.uisil.smartpeople.estructura;
+
+/**
+ *
+ * @author Bernal Santiago
+ */
+public class Impuesto {
+    
+    private long id;
+    private int codigo;
+    private String nombre;
+    private float porcentaje;
+    private boolean activo;
+    
+}
