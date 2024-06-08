d7d35a44b43a0d0650d8c2122ac2b3e1e0819f02
TryingToBeComeAProgrammer000 <uyv310599@gmail.com>
2024-06-08 15:01:20 -0600

    primer commit

---
+++ src/main/java/cr/ac/uisil/smartpeople/SmartPeople.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cr.ac.uisil.smartpeople;
+
+/**
+ *
+ * @author Bernal Santiago
+ */
+public class SmartPeople {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
